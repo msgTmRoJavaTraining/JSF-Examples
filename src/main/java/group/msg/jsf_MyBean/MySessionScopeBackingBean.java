3af992e5d1c825a97ecedb70ba4d191a0b4080e2
bencehach <bencehach@gmail.com>
2019-07-19 16:07:49 +0300

    Show an error message if rating is not a number

--- src/main/java/group/msg/jsf_MyBean/MySessionScopeBackingBean.java
+++ src/main/java/group/msg/jsf_MyBean/MySessionScopeBackingBean.java
@@ -3,6 +3,8 @@ package group.msg.jsf_MyBean;
 import lombok.Data;
 
 import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -13,12 +15,17 @@ import java.util.ArrayList;
 public class MySessionScopeBackingBean implements Serializable
 {
     private String savedData ;
-    private int rating;
+    private String rating;
     private String outputMessage = "";
 
 
     public void saveInput()
     {
+        if(rating.contains("."))
         outputMessage +=" "+savedData+" "+ rating;
+        else
+        {
+            FacesContext.getCurrentInstance().addMessage(null,new FacesMessage("Error","Rating must be decimal a number"));
+        }
     }
 }
