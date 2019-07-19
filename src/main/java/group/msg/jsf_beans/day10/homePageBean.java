098aa153943b42cb0e217a659274c55c4bfb141d
Andrei Astanei <andreihao@gmail.com>
2019-07-19 16:17:27 +0300

    Clasa java corespunzatoare paginii de home

---
+++ src/main/java/group/msg/jsf_beans/day10/homePageBean.java
@@ -0,0 +1,37 @@
+package group.msg.jsf_beans.day10;
+
+import group.msg.WebHelper;
+import group.msg.jsf_beans.LoginBackingBean;
+import lombok.Data;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.jms.JMSException;
+import javax.jms.Session;
+import java.io.Serializable;
+
+@RequestScoped
+@Named
+@Data
+public class homePageBean implements Serializable {
+    @Inject
+    private MyLoginBean myLoginBean;
+
+    private String loggedInUserName;
+
+    @PostConstruct
+    public void init(){
+        loggedInUserName = myLoginBean.getCurrentlyLoggedInUsername();
+    }
+
+    public String navigateTo(String page){
+        return page;
+    }
+
+    public String logUserOut() {
+        WebHelper.getSession().setAttribute("loggedIn",false);
+        return "my_login";
+    }
+}
