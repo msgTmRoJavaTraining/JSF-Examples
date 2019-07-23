e7f7a0fa34aa7901a079a080a8923209a050cb8b
Andrei Astanei <andreihao@gmail.com>
2019-07-23 15:53:13 +0300

    Clasa pentru obiectele de tip Employee folosite de aplicatie

---
+++ src/main/java/group/msg/entities/Employee.java
@@ -0,0 +1,14 @@
+package group.msg.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+    private String name;
+    private String role;
+    private String cnp;
+}
